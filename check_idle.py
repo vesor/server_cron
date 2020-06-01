1e696070d840b483a5ec703fa2a41e345965b2e4
weizhe <weizheliu@telenav.com>
2020-06-01 10:57:12 +0800

    better wakeup check

--- check_idle.py
+++ check_idle.py
@@ -1,4 +1,11 @@
 
+'''
+use "sudo crontab -e" to add a task for this script:
+for example, every 5 min:
+
+*/5 * * * * sudo python3 /media/data/server_cron/check_idle.py
+
+'''
 
 import sys, os
 import subprocess
@@ -6,9 +13,12 @@ import logging
 from logging import handlers
 from datetime import datetime, timedelta
 import re
-import dateutil.parser
+import dateutil.parser, time
 
-work_dir = os.path.dirname(sys.argv[0])
+work_dir = os.path.join(os.path.dirname(sys.argv[0]), 'data')
+if not os.path.exists(work_dir):
+    os.makedirs(work_dir)
+
 script_name = os.path.basename(sys.argv[0])
 
 logger = logging.getLogger(script_name)
@@ -20,44 +30,32 @@ hdlr.setFormatter(formatter)
 logger.addHandler(hdlr) 
 logger.setLevel(logging.INFO)
 
-def get_load_avg():
-    f = open("/proc/loadavg")
-    loadavg = f.read().split()
-    f.close()
-    return loadavg[:3] # load avg of 1, 5, 15min
 
 def is_idle():
     loadavg = os.getloadavg()
     logger.info('getloadavg: %s', loadavg)
-    load_thresh = 0.5
+    load_thresh = 1.5
     idle = (loadavg[0] < load_thresh and loadavg[1] < load_thresh and loadavg[2] < load_thresh)
     return idle
 
+# lock (disable) idle checking. no sleep at all. 
+def is_locked():
+    return os.path.exists(os.path.join(work_dir, 'lock'))
+
+# check this to avoid sleep immediately after wakeup
 def is_wakeup_recently():
-    last_wakeup_log = None
-    with open('/var/log/kern.log') as logfile:
-        for line in logfile:
-            if line.find(' PM: suspend exit') != -1:
-                last_wakeup_log = line
+    try:
+        waketime_ts = os.path.getmtime(os.path.join(work_dir, 'wakeup'))
+        waketime = datetime.fromtimestamp(waketime_ts)
+    except:
+        return False
     
-    if last_wakeup_log is None:
-        return False
-
-    #print('log:',line)
-    re_obj = re.search('^[^ ]+ [^ ]+ [^ ]+ ', last_wakeup_log)
-    if re_obj is None:
-        return False
-
-    #print('re', re_obj.group(), '$')
-    event_time = dateutil.parser.parse(re_obj.group())
-    if event_time is None:
-        return False
-
-    #print('dt',event_time)
-    delta = datetime.now()-event_time
+    #print ('waketime', waketime)
+    
+    delta = datetime.now()-waketime
     #print('delta', delta)
 
-    logger.info('is_wakeup_recently: %s,%s', event_time, delta)
+    logger.info('is_wakeup_recently: %s,%s', waketime, delta)
 
     if delta > timedelta(minutes=5):
         return False
@@ -92,26 +90,31 @@ def time_to_next_wakeup(now):
 
     return time_to_next
 
-def rtcwake(seconds):
-    #cmd = ['ls', '-l', 'abcde']
-    cmd = ['rtcwake', '-m', 'mem', '-s', str(int(seconds))]
+def run_cmd(cmd):
     logger.info('subprocess.Popen: %s', cmd)
     MyOut = subprocess.Popen(cmd, 
             stdout=subprocess.PIPE, 
             stderr=subprocess.STDOUT)
     stdout,stderr = MyOut.communicate()
-    logger.info('subprocess.Popen stdout: %s', stdout)
-    logger.info('subprocess.Popen stderr: %s', stderr)
+    if stdout is not None and len(stdout) > 0:
+        logger.info('subprocess.Popen stdout: %s', stdout.decode("utf-8"))
+    if stderr is not None and len(stderr) > 0:
+        logger.info('subprocess.Popen stderr: %s', stderr.decode("utf-8"))
     #print(stdout.decode("utf-8"))
     #print(stderr)
+
+def rtcwake(seconds):
+    #cmd = ['ls', '-l', '.']
+    run_cmd(['rtcwake', '-m', 'mem', '-s', str(int(seconds))])
+    run_cmd(['touch', os.path.join(work_dir, 'wakeup')])
 
 def main():
     #now = datetime(2020, 5, 30, 22, 55, 12)
     #time_to_next_wakeup(now)
-    if is_idle() and not is_wakeup_recently():
+    if not is_locked() and is_idle() and not is_wakeup_recently():
         now = datetime.now()
         sec = time_to_next_wakeup(now)
-        rtcwake(sec)
+        rtcwake(10)
 
 if __name__ == "__main__":
     main()
